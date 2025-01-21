ff97a90f8b712d944e24a252fcff24f1365ff1c5
mahmouudKhalled <foktop018@gmail.com>
2025-01-21 03:22:52 +0200

    modify logout file

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,16 +1,21 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function POST() {
   // Create a response to indicate successful logout
-  const response = NextResponse.json({ message: 'Logged out successfully' });
+  const response = NextResponse.json({ message: "Logged out successfully" });
 
   // Remove the 'next-auth.session-token' cookie by setting it with an expired date
-  response.cookies.set('next-auth.session-token', '', {
-    path: '/', 
-    expires: new Date(0), 
+  const sesstionName =
+    process.env.NODE_ENV === "production"
+      ? "__Secure-next-auth.session-token"
+      : "next-auth.session-token";
+
+  response.cookies.set(sesstionName, "", {
+    path: "/",
+    expires: new Date(0),
     httpOnly: true,
     secure: true,
-    sameSite: 'strict',
+    sameSite: "strict",
   });
 
   // Return the response
